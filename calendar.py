9e25f3c0c7604871aa00b1de9354d2c4510a6e40
Mithleshk595 <mithleshk0982@gmail.com>
2025-08-31 14:26:45 +0530

    added calendar.py

---
+++ calendar.py
@@ -0,0 +1,29 @@
+# import calendar # calendar module import karna h
+
+# # input year and month
+# year = int(input("enter year:"))
+# month = int(input("enter month (1-12): "))
+
+# #print the calendar for given month
+# print("\n", calendar.month(year, month))
+
+
+# import calendar # calendar module import karna h
+
+# # input year and month
+# year = int(input("enter year:"))
+# month = int(input("enter month (1-12):"))
+
+
+# #print thr calendar for given month
+
+# print("\n", calendar.month(year, month))
+
+import calendar   # calendar module import karna h
+
+# input year and month
+year = int(input("Enter year: "))
+month = int(input("Enter month (1-12): "))
+
+# print the calendar for given month
+print("\n", calendar.month(year, month))
