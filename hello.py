af6b5852dea528041fa1caf1f1ae8679c7d9c07f
Mithleshk595 <mithleshk0982@gmail.com>
2025-09-02 22:25:55 +0530

    Added hello.py

---
+++ hello.py
@@ -0,0 +1,46 @@
+# print("hello word!")
+
+# a = int(input("Enter first number :"))
+# b = int(input("Enter second number :"))
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a % b)
+
+# length = float(input("enter the length of the rectangle :"))
+# width = float(input("enter the width of the rectangle :"))
+
+# area = length * width
+
+# print("the area of the rectangle is:", area)
+
+# radius = float(input("Enter the radius of the circle :"))
+
+# area = max.pi * radius ** 2
+
+# circumference = 2 * max.pi * radius
+
+# print("Area of the circle is :", area)
+# print("Circumference of the circle is :", circumference)
+
+# P = float(input("Enter principle amount :"))
+# R = float(input("Enter rate of interest :"))
+# T = float(input("Enter time in years :"))
+
+# SI = P * R * T / 100
+
+# print("simple interest", SI)
+
+
+
+num= int(input("Entera number :"))
+
+
+
+if num % 2 == 0:
+    print(num, "is Even :")
+else:
+    print(num, "is Odd :")
+
