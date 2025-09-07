8f75694144d2018109a3a3cf9a83ed388c298149
Mithleshk595 <mithleshk0982@gmail.com>
2025-09-07 09:58:04 +0530

    Added program_true.py

---
+++ program_true.py
@@ -0,0 +1,33 @@
+s1 = 'NitiAayog'
+s2 = 'And Quit Flow The Don'
+s3 = '1234567890'
+s4 = 'Make $1000 a day'
+
+print('s1 = ', s1)
+print('s2 = ', s2)
+print('s3 =',  s3)
+print('s4 =', s4)
+
+#content test fundtion 
+
+print('check isalpha om s1, s2')
+print(s1 .isalpha())
+print(s2 .isalpha())
+
+print('check isdigit on s3, s4')
+print(s3. isdigit)
+print(s4 .isdigit)
+
+print('check isalnum on s1, s2, s3, s4')
+print(s1.isalnum)
+print(s2.isalnum)
+print(s3.isalnum)
+print(s4.isalnum)
+
+print('check islower on s1, s2')
+print(s1.islower)
+print(s2.islower)
+
+print('check startwith and endswith on s2')
+print(s2.startswith('And'))
+print(s2.endswith('And'))
