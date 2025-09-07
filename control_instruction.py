44fa5bb8042e6e821d2b3909e222b3e171676ec2
Mithleshk595 <mithleshk0982@gmail.com>
2025-09-07 22:58:22 +0530

    Added control_instruction.py

---
+++ control_instruction.py
@@ -0,0 +1,41 @@
+# i, j, k = 4, -1, 0
+# w = i or j or k
+# x = i and j or k 
+# y = i or j and k
+# z = i and j or k
+
+# print(w, x, y, z)
+
+# a = 10
+# a = not not a 
+# print(a)
+
+
+# x, y, z = 20, 40, 45
+# if x > y and x > z:
+#     print("biggest=" + str(x))
+# elif y > x and y > z:
+#     print("biggrst=" + str(y))
+# elif z > x and z > y:
+#     print("biggest=" +str(z))
+
+# num = 30
+# k = 100 if num <= 10 else 500
+# print(k)
+
+
+# a = 10 
+# b = 60
+# if a and b > 20:
+#     print("hello")
+# else:
+#     print("hii")
+
+
+a = 10
+if a == 30 or a == 40 or a == 60:
+    print("hello")
+else:
+    print("hii")
+
+
