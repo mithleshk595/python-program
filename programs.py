34159b518dfe7d705c8c5c2f49d9f3dc3b5a6c75
Mithleshk595 <mithleshk0982@gmail.com>
2025-09-08 22:13:38 +0530

    Added programs.py

---
+++ programs.py
@@ -0,0 +1,15 @@
+# while True:
+#     print("infinite loop")
+
+
+i = 1
+while i <= 10:
+    j =1
+    while j <= 5:
+        print(i, j)
+        j +=1
+        break
+    print(i, j)
+    i += 1
+
+
