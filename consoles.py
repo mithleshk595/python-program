2fb3635756e7b44b017a2b6a35c08c001986d0f7
Mithleshk595 <mithleshk0982@gmail.com>
2025-09-08 22:43:57 +0530

    consoles.py

---
+++ consoles.py
@@ -0,0 +1,19 @@
+# contacts = {
+#     "Mithlesh" : 7366866893, "Shekhar" : 9525439810, "Viviek" : 6560554540, "Ridhi" : 9798123456
+
+# }
+
+# for name, cellno in contacts.items():
+#     print(f"{name:15} : {cellno:10d}")
+
+
+min, max = 25, 75
+mean = 35
+sd = 0.56
+var = 0.9
+print(f"n{"Max Value:":<15}{max:>10}"),
+f"\n{"Min Value:":<15}{min:>10}",
+f"\n{"Mean:":<15}{mean:>10}",
+f"\n{"Std Dev:":<15}{sd:>10}",
+f"\n{"Variance:":<15}{var:>10}",
+
