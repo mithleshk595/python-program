2890d4357819e34f058d0f9a77d9a1955577e7bd
Mithleshk595 <mithleshk0982@gmail.com>
2025-09-09 21:59:05 +0530

    Added list.advance.py

---
+++ list.advance.py
@@ -0,0 +1,16 @@
+import collections
+q = collections.deque()
+
+q.append("Suhana")
+q.append("Shakira")
+q.append("Shakila")
+q.append("Sameera")
+q.append("sahiba")
+print(q)
+
+print(q.popleft())
+print(q.popleft())
+print(q.popleft())
+print(q)
+
+
