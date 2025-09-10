3994999903ff2e70fe88a14f897df9a83697290e
Mithleshk595 <mithleshk0982@gmail.com>
2025-09-10 21:48:20 +0530

    Added setss.py

---
+++ setss.py
@@ -0,0 +1,23 @@
+s = {10, 2, 3, 4, 5, 88}
+print(len(s))
+print(max(s))
+print(max(s))
+print(sum(s))
+t =sorted()
+print(t)
+print(100 in s)
+print(-3 not in s)
+
+i = [10, 20, 30, 40, 50]
+t = ("Sundeep", 25, 79, 58)
+s = "set theory"
+s1 = set(i)
+s2 = set(t)
+s3 = set(s)
+print(s1)
+print(s2)
+print(s3)
+
+s1 = {10, 20, 30, 40, 50}
+s2 = {10, 20, 30, 40, 50}
+print(id(s1), id(s2))
