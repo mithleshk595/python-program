4e60fa839b6aa0188c627663641dc84900fdb05a
Mithleshk595 <mithleshk0982@gmail.com>
2025-09-11 09:44:30 +0530

    Added functions.py

---
+++ functions.py
@@ -0,0 +1,18 @@
+# def cal_sum_prod(x, y, z):
+#     ss = x + y + z
+#     pp = x * y * z
+#     return ss, pp
+
+# a = int(input("Enter a: "))
+# b = int(input("Enter b: "))
+# c = int(input("Enter c: "))
+# s, p = cal_sum_prod(a, b, c)
+# print(s, p)
+
+def generate_list():
+    lst = list()
+    for i in range(1, 11):
+        lst-list.append(i, i**2, i **3)
+    return lst
+i = generate_list()
+print(i)
